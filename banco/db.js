2535b3e275217128619109c76563e9b85e5f40c8
lahbrito18 <134542731+lahbrito18@users.noreply.github.com>
2025-05-27 15:32:40 -0400

    Update db.js

--- banco/db.js
+++ banco/db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./banco.db');
+const db = new sqlite3.Database('./banco/banco.db');
 
 // Criar tabelas
 db.serialize(() => {
