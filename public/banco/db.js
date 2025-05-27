3080b701d78ae0b9dd102fe8c929888ac0709f9b
lahbrito18 <134542731+lahbrito18@users.noreply.github.com>
2025-05-27 13:54:06 -0400

    Add files via upload

---
+++ public/banco/db.js
@@ -0,0 +1,62 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./banco.db');
+
+// Criar tabelas
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS questions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    enunciado TEXT NOT NULL,
+    categoria TEXT,
+    banca TEXT,
+    ano INTEGER,
+    comentario TEXT
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS alternativas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question_id INTEGER,
+    texto TEXT,
+    correta BOOLEAN,
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS salas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    creator_id INTEGER,
+    FOREIGN KEY(creator_id) REFERENCES users(id)
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS sala_usuarios (
+    sala_id INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY(sala_id) REFERENCES salas(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS simulados (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sala_id INTEGER,
+    user_id INTEGER,
+    pontuacao INTEGER,
+    data DATE
+  )`);
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS comentarios (
+  id SERIAL PRIMARY KEY,
+  questao_id INT NOT NULL,
+  usuario_id INT NOT NULL,
+  nome_usuario VARCHAR(100),
+  texto TEXT NOT NULL,
+  data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )`);
+  
+module.exports = db;
